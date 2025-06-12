c72199dafc387bb546024f45e26b6b8529f8535a
Shiva-Chaurasiya-01 <shivachaurasiya.9298@gmail.com>
2025-06-12 19:36:29 +0530

    Add Temperature Converter Project

--- Temperature Converter/script.js
+++ Temperature Converter/script.js
@@ -2,20 +2,26 @@ console.log("Enter temperature you want to change : Celcius(c) \t Fahrenheit(f)"
 
 let unit = prompt("Enter Degree :");
 let temp = Number(prompt("Enter Your Temperature :"));
+
+
 let userunit = unit.toLowerCase();
 let userTemp = temp;
+
 
 if (!unit || isNaN(userTemp)) {
     console.log(`Error! You have entered wrong Temperature.`)
 }
 
+
 else if (userunit === "c" && userTemp <= -273.15) {
     console.log(`Error! The Temperature you have entered is Lower than Absolute Zero`)
 }
 
+
 else if (userunit === "f" && userTemp <= -459.67) {
     console.log(`Error! The Temperature you have entered is Lower than Absolute Zero`)
 }
+
 
 else {
 
@@ -26,6 +32,7 @@ else {
 
     }
 
+
     else if (userunit === "f") {
 
         let Celsius = Number(((userTemp - 32) * 5 / 9).toFixed(2));
@@ -33,6 +40,7 @@ else {
 
     }
 
+ 
     else {
 
         console.log(`Error!  You must enter "C" or "F"`);
